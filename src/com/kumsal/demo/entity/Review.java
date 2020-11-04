56cbc868e5932d0905ce7fb0370c00ee715e39da
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-04 15:35:04 +0300

    add annotatıon and column name

--- src/com/kumsal/demo/entity/Review.java
+++ src/com/kumsal/demo/entity/Review.java
@@ -1,17 +1,31 @@
 package com.kumsal.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GeneratorType;
+
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 @Entity
 @Table(name="review")
 public class Review {
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
 	@Column(name="comment")
 	private String Comment;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="")
+	private Course course;
 	
 }
