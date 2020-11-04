189a9ddd548ead37dd0ff66fa2004dc93e2545ef
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-04 15:29:38 +0300

    add new fıeld and annotatıon

--- src/com/kumsal/demo/entity/Review.java
+++ src/com/kumsal/demo/entity/Review.java
@@ -1,5 +1,17 @@
 package com.kumsal.demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+@Entity
+@Table(name="review")
 public class Review {
-
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="comment")
+	private String Comment;
+	
 }
